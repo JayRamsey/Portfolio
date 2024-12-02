70b1b62abe976dc9bcd0bfc53127e7cf29fa0fef
Jay Ramsey <159651134+JayRamsey@users.noreply.github.com>
2024-12-02 10:47:17 +0000

    Create pointers.cpp

---
+++ Computer Science/C++/pointers.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+int main() {
+    // pointers
+    int x = 4;
+    int *y = &x;
+    
+    cout << *y << endl;
+
+    return 0;
+}
